d8167a67edb5d1b749ad4636f1433ceebc722266
Devendra Thakare <devendra.thakare@erosnow.com>
2019-03-11 18:21:50 +0530

    Enabled CORS

--- server.js
+++ server.js
@@ -19,37 +19,42 @@ const oauth = OAuth({
     }
 })
 
-// TODO: Remove HardCoded token
-// const token = {
-//     key: '0d5e96040fd071a6282a44be1ad0c7ec05c6d30c6',
-//     secret: '6dea50d55bef78fe5f66bb19f578cfc4'
-// }
-
 const onRequest = (clientRequest, clientResponse) => {
-    console.log('serve: ' + clientRequest.url)
-    const address = url.parse(clientRequest.url, true)
-    // const queryString = address.query
-    const requestUrl = `${config.apiBaseUrl}${clientRequest.url}`
-    const requestData = {
-        url: requestUrl,
-        method: clientRequest.method
-        // qs: queryString
-    }
-    request({
-        ...requestData,
-        // headers: oauth.toHeader(oauth.authorize(requestData, token))
-        headers: oauth.toHeader(oauth.authorize(requestData))
-        // qs: queryString,
-        // headers: {
-        //     // ...clientRequest.headers,
-        //     Authorization: oauth.toHeader(oauth.authorize(requestData, token))
-        //         .Authorization
-        // }
-    })
-        .on('error', function(e) {
-            clientResponse.end(e)
+    clientResponse.setHeader('Access-Control-Allow-Origin', '*')
+    clientResponse.setHeader(
+        'Access-Control-Allow-Methods',
+        'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+    ) // If needed
+    clientResponse.setHeader(
+        'Access-Control-Allow-Headers',
+        'X-Requested-With,content-type'
+    ) // If needed
+    clientResponse.setHeader('Access-Control-Allow-Credentials', true) // If needed
+
+    // intercept OPTIONS method
+    if (clientRequest.method == 'OPTIONS') {
+        clientResponse.statusCode = 200
+        clientResponse.end()
+    } else {
+        console.log('serve: ' + clientRequest.url)
+        const address = url.parse(clientRequest.url, true)
+        // const queryString = address.query
+        const requestUrl = `${config.apiBaseUrl}${clientRequest.url}`
+        const requestData = {
+            url: requestUrl,
+            method: clientRequest.method
+            // qs: queryString
+        }
+        request({
+            ...requestData,
+            // headers: oauth.toHeader(oauth.authorize(requestData, token))
+            headers: oauth.toHeader(oauth.authorize(requestData))
         })
-        .pipe(clientResponse)
+            .on('error', function(e) {
+                clientResponse.end(e)
+            })
+            .pipe(clientResponse)
+    }
 }
 const port = process.env.PORT || 4000
 http.createServer(onRequest).listen(port)
